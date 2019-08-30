b971499433cbaf106c238966c9aaee60824c7b26
Arlene Andrews <kara_arlene@yahoo.com>
2019-08-30 11:53:00 -0600

    BROKEN: Trying different things

--- Hello-world/tests/part1_test.py
+++ Hello-world/tests/part1_test.py
@@ -2,12 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# create a new Firefox session
+driver = webdriver.Firefox(executable_path='E:\Documents and Settings\mom\geckodriver-v0.24.0-win64')
+driver.implicitly_wait(30)
+driver.maximize_window()
 
 class TestFindItem():
 
     @pytest.fixture
     def driver(self, request):
-        driver_ = webdriver.Chrome()
+        driver_ = driver
 
         def quit():
             driver_.quit()
