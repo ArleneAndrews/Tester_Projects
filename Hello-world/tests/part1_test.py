32c11509813f5df5af72d030eda6c6124388dd0e
Arlene Andrews <kara_arlene@yahoo.com>
2019-09-02 13:38:52 -0600

    WORKS! Importing Keys, and getting to correct page

--- Hello-world/tests/part1_test.py
+++ Hello-world/tests/part1_test.py
@@ -1,6 +1,7 @@
 import pytest, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # create a new Chrome session
 driver = webdriver.Chrome()
@@ -21,5 +22,5 @@ class TestFindItem():
     def test_enter_type(self, driver):
         driver.get("https://www.iceyarns.com/")
         driver.find_element(By.ID, "sinput2").send_keys("eyelash")
-        driver.find_element(By.ID, "sinput2").send_keys(keys.RETURN)
+        driver.find_element(By.ID, "sinput2").send_keys(Keys.RETURN)
         time.sleep(2) # Let the user actually see something!
