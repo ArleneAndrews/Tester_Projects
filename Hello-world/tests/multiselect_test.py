f61125e86a4bafd978685139d181613fbc616be5
Arlene Andrews <kara_arlene@yahoo.com>
2019-09-09 16:10:10 -0600

    ERROR: Cleanup and new test

---
+++ Hello-world/tests/multiselect_test.py
@@ -0,0 +1,32 @@
+import pytest, time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# create a new Chrome session
+driver = webdriver.Chrome()
+
+class TestFindItem():
+
+    @pytest.fixture
+    def driver(self, request):
+        driver_ = driver
+
+        def quit():
+            driver_.quit()
+
+        request.addfinalizer(quit)
+        return driver_
+
+
+    def test_enter_type(self, driver):
+        driver.get("https://www.iceyarns.com/")
+        driver.find_element(By.ID, "sinput2").send_keys("eyelash yarns" + Keys.RETURN)
+        driver.find_element_by_class_name ("tag-discount").click()
+        form_element = driver.find_element_by_xpath("//form[@class='productcart']/input[1]").click()
+        time.sleep(2) # Let the user actually see something!
+        driver.close()
+
+    def test_find_many(self, driver)
+        driver.get("https://www.scientificsonline.com/shop/geek-shop")
+        driver.find_element_by_class_name(".badge-Promotional")
