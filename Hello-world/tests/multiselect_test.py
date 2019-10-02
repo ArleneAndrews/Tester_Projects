e679ee28ec71906bd452944ff9e7846bda6d0bf4
Arlene Andrews <kara_arlene@yahoo.com>
2019-10-02 14:02:13 -0600

    BROKEN working on a check for no items

--- Hello-world/tests/multiselect_test.py
+++ Hello-world/tests/multiselect_test.py
@@ -2,6 +2,7 @@ import pytest, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from collections import Counter
 
 # create a new Chrome session
 driver = webdriver.Chrome()
@@ -20,16 +21,21 @@ class TestFindItem():
 
     def emptycart():
         driver.find_element_by_class_name ("boxshopcart").click()
-        driver.find_element_by_class_name ("del").click()
+        driver.find_element_by_link_text ("Empty Cart").click()
+
+    def noitems():
         
-     
-    #def noitems()
-
-    #def 
-
+        for item in 
+        driver.find_element_by_class_name ("tag-discount")
+	     
     def test_find_many(self, driver)
+        emptycart()
         driver.get("https://www.iceyarns.com/")
+        Count= 19 # There are 19 items on the opening page + "Show More"
+        noitems(Count)
         driver.find_element_by_class_name ("tag-discount").click()
+
         #form_element = driver.find_element_by_xpath("//form[@class='productcart']/input[1]").click()
         time.sleep(2) # Let the user actually see something!
+        emptycart()
         driver.close()
